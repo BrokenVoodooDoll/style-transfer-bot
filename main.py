c9203031c9e72a50c2b840b7a0326a692ffe0fd9
BrokenVoodooDoll <vinmax96@gmail.com>
2022-07-05 23:16:12 +0300

    Added style transfer module

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from PIL import Image
+from style_transfer import style_transfer
+from enum import Enum
+
+
+class State(Enum):
+    IDLE = 0
+    CHOOSING_CONTENT = 1
+    CHOOSING_STYLE = 2
+
+state = State
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,14 +19,21 @@ logging.basicConfig(
 )
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Send an image to me")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+        text="This bot transfers style from one photograph to another. \
+        Send me two photos: a content image and a style image. \
+        The order is important!")
 
 async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.message.photo)
+
     file = await update.message.photo[-1].get_file()
     await file.download("./photo.jpeg")
 
-    # here the network starts
-    
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Please wait. This may take a while...")
+    # await style_transfer('figures.jpg', 'vg_starry_night.jpg')
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Got it")
+
 
 if __name__ == '__main__':
     with open('token.txt', 'rt') as f:
